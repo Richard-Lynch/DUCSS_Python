4270392092ec534bacc22be55f27900a99b93a6c
Richard-Lynch <rich753@hotmail.com>
2017-02-16 18:56:58 +0000

    this is class

--- main.py
+++ main.py
@@ -34,8 +34,44 @@ if __name__ == "__main__": #treats this as main? maybe i think
     print ("some string[-2]", some_string2[-2])
     print ("some string[::-1]", some_string2[::-1]) #-1 is step
 
-    
     print (some_int)
     print (some_float)
     print (some_bool)
     print (some_null)
+
+    aint = 11
+    print ("number: ", aint)
+    print ("adding 2: ", aint + 2)
+    print ("subing 2: ", aint -2)
+    print ("mult 2: ", aint * 2)
+    print ("div 2: ", aint /2)
+    print ("pow 2: ", aint ** 2)
+
+    print ("checking int: {}", format(type(aint) is int) )
+    print ("checking int: {}".format(type(aint) is int) )
+
+    aint += 2
+    print ("+= 2:", aint )
+
+    print ("some_bool", some_bool)
+    print ("not_some_bool", not some_bool)
+
+    try:
+        print("a/b: ",some_int/aint)
+    except:
+        print("issues")
+    
+    aint = 0
+    
+    try:
+        print("a/b: ",some_int/aint)
+    except Exception as e:              #Exception is the default error, saved as e, and then printed
+        print("issues: ", e)
+        # raise  # will crash 
+    else:                               #if no error
+        print ("no issues")
+    finally:                            #either way 
+        print ("moving on")             #this is for when youve dealt with soemthing, and jsut want to tidy up? I think
+
+    
+
